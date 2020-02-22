2fa9a43225ed4f0f168766cb9ef7d973bc6b2422
Dimitris Christodoulou <demetrischr1998@gmail.com>
2020-02-22 14:21:47 +0000

    Do not track book if it is not found

--- scripts/alignment.py
+++ scripts/alignment.py
@@ -6,7 +6,8 @@ def alignment(lcc_code, camera_idx=0):
     booksimg = BooksImage(camera_idx=camera_idx)
     booksimg.preprocessAndReadLabels()
     label_rectangle = findBook(booksimg, lcc_code)
-    center_spine(label_rectangle, camera_idx)
+    if label_rectangle is not None:
+        center_spine(label_rectangle, camera_idx)
 
 
 if __name__ == '__main__':
