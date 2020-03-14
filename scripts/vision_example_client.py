8325676fa1d892b152525c075d744fa3c0cf97b9
Dimitris Christodoulou <s1738739@ed.ac.uk>
2020-03-14 14:05:57 +0000

    Change host to the raspberry pi 4

--- scripts/vision_example_client.py
+++ scripts/vision_example_client.py
@@ -4,13 +4,13 @@ import json
 labels = ['DG311 Gib.', 'BJ1499.S5 Kag.', 'QC21.3 Hal.', 'QC174.12 Bra.', 'PS3562.E353 Lee.',
           'PR4662 Eli.', 'HA29 Huf.', 'QA276 Whe.', 'QA76.73.H37 Lip.', 'QA76.62 Bir.']
 
-target_label = 'DG311 Gib.'
+target_label = 'PR4662 Eli.'
 
 shelf = 0
 
 to_send = {'target_label' : target_label, 'labels': labels, 'shelf': shelf}
 
-HOST = 'middleton'
+HOST = '192.168.105.3'
 PORT = 50000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
